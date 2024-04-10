9dff9a61771aad64458dbf85270a8624d4ae8f4b
giannisaladino <saladinogianni6@gmail.com>
2024-04-10 15:47:53 +0200

    EDIT: style in App to general.scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,19 +1,32 @@
 <script>
-    export default {
-        data () {
-            
-        }
+export default {
+    data() {
+
     }
+}
 </script>
 
 <template>
-      <footer class="page-footer">
-    <div class="container">
-      <div class="row">
-        <p>Booleando s.r.l</p>
-        <p>Lorem, ipsum dolor.</p>
-        <h4>Trovaci anche su</h4>
-      </div>
-    </div>
-  </footer>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <p>Booleando s.r.l</p>
+                <p>Lorem, ipsum dolor.</p>
+                <h4>Trovaci anche su</h4>
+            </div>
+        </div>
+    </footer>
 </template>
+
+<style lang="scss" scoped>
+.page-footer {
+    background-color: black;
+    color: white;
+    margin-top: 35px;
+    padding: 15px;
+}
+
+.page-footer .row {
+    justify-content: space-between;
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,29 +1,29 @@
 <script>
-    export default {
-        data () {
-            return {
+export default {
+    data() {
+        return {
 
-            }
         }
     }
+}
 </script>
 
 <template>
     <header class="page-header">
-    <nav class="container">
-      <div class="row">
-        <ul class="flex">
-          <li>Donna</li>
-          <li>Uomo</li>
-          <li>Bambini</li>
-        </ul>
-        <img class="logo-header" src="./images/img/boolean-logo.png" alt="">
-        <ul class="flex">
-          <li>&hearts;</li>
-          <li>&hearts;</li>
-          <li>&hearts;</li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+        <nav class="container">
+            <div class="row">
+                <ul class="flex">
+                    <li>Donna</li>
+                    <li>Uomo</li>
+                    <li>Bambini</li>
+                </ul>
+                <img class="logo-header" src="./images/img/boolean-logo.png" alt="">
+                <ul class="flex">
+                    <li>&hearts;</li>
+                    <li>&hearts;</li>
+                    <li>&hearts;</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 </template>
