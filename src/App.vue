5ae35794db5bbaac9ed73fe44fc5fa4cc246acb0
giannisaladino <saladinogianni6@gmail.com>
2024-04-10 14:47:13 +0200

    ADD: html and css by "html-css-boolando"

---
+++ src/App.vue
@@ -0,0 +1,304 @@
+<script></script>
+
+<template>
+  <header class="page-header">
+    <nav class="container">
+      <div class="row">
+        <ul class="flex">
+          <li>Donna</li>
+          <li>Uomo</li>
+          <li>Bambini</li>
+        </ul>
+        <img class="logo-header" src="./images/img/boolean-logo.png" alt="">
+        <ul class="flex">
+          <li>&hearts;</li>
+          <li>&hearts;</li>
+          <li>&hearts;</li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <main class="page-content">
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <img src="./images/img/1.webp" alt="">
+                <span class="price-badge">-50 %</span>
+                <span class="tag-badge">Sostenibilità</span>
+                <span class="heart-badge">&hearts;</span>
+                <div class="overlay">
+                  <img src="./images/img/1b.webp" alt="">
+                </div>
+              </div>
+              <div class="card-body">
+                <p>Levi's</p>
+                <p><strong>RELAXED FIT TEE UNISEX</strong></p>
+                <p class="red-price"><strong>14,99&euro;</strong></p><span class="before-price">29,99&euro;</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card product">
+              <div class="card-header">
+                <img src="./images/img/2.webp" alt="">
+                <span class="price-badge">-30 %</span>
+                <span class="heart-badge">&hearts;</span>
+                <div class="overlay">
+                  <img src="./images/img/2b.webp" alt="">
+                </div>
+              </div>
+              <div class="card-body">
+                <p>Guess</p>
+                <p><strong>ROSES TEE</strong></p>
+                <p class="red-price"><strong>20,99&euro;</strong></p><span class="before-price">29,99&euro;</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card product">
+              <div class="card-header">
+                <img src="./images/img/3.webp" alt="">
+                <span class="price-badge">-30 %</span>
+                <span class="heart-badge">&hearts;</span>
+              </div>
+              <div class="card-body">
+                <p>Come Zucchero Filato</p>
+                <p><strong>VOGLIA DI COLORI PASTELLO</strong></p>
+                <p class="red-price"><strong>129,99&euro;</strong></p><span class="before-price">184,99&euro;</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card product">
+              <div class="card-header">
+                <img src="./images/img/4.webp" alt="">
+                <span class="price-badge">-50 %</span>
+                <span class="tag-badge">Sostenibilità</span>
+                <span class="heart-badge">&hearts;</span>
+                <div class="overlay"><img src="./images/img/4b.webp" alt=""></div>
+              </div>
+              <div class="card-body">
+                <p>Levi's</p>
+                <p><strong>TEE UNISEX</strong></p>
+                <p class="red-price"><strong>14,99&euro;</strong></p><span class="before-price">29,99&euro;</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card product">
+              <div class="card-header">
+                <img src="./images/img/5.webp" alt="">
+                <span class="heart-badge">&hearts;</span>
+                <div class="overlay"><img src="./images/img/5b.webp" alt=""></div>
+              </div>
+              <div class="card-body">
+                <p>Maya Deluxe</p>
+                <p><strong>STRIPE BODICE</strong></p>
+                <p class="red-price"><strong>99,99&euro;</strong></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card product">
+              <div class="card-header">
+                <img src="./images/img/6.webp" alt="">
+                <span class="tag-badge only">Sostenibilità</span>
+                <span class="heart-badge">&hearts;</span>
+                <div class="overlay"><img src="./images/img/6b.webp" alt=""></div>
+              </div>
+              <div class="card-body">
+                <p>Esprit</p>
+                <p><strong>MAGLIONE - BLACK</strong></p>
+                <p class="red-price"><strong>29,99&euro;</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <footer class="page-footer">
+    <div class="container">
+      <div class="row">
+        <p>Booleando s.r.l</p>
+        <p>Lorem, ipsum dolor.</p>
+        <h4>Trovaci anche su</h4>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style>
+/* RESET */
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+img {
+	max-width: 100%;
+}
+
+a {
+	text-decoration: none;
+	color: currentColor;
+}
+
+menu,
+ul,
+ol {
+	list-style: none;
+}
+
+/* LAYOUT */
+
+.container {
+	max-width: 1024px;
+	margin: 0 auto;
+	padding: 0 24px;
+}
+
+.row {
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.col-4 {
+	width: calc(4 * 100% / 12);
+    padding-top: 30px; 
+    padding-left: 7px;
+    padding-right: 7px;
+}
+
+.col-50 {
+    width: calc(100 / 2);
+    display: flex;
+    justify-content: space-between;
+}
+
+.row ul {
+    gap: 10px;
+}
+
+/* STYLE */
+
+.page-header {
+    background-color: #FF6801;
+    color: white;
+}
+
+.page-header .row {
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+}
+
+.logo-header {
+    height: 25px;
+}
+
+.red-price {
+    display: inline-block;
+    color: red;
+    padding-right: 4px;
+}
+
+.before-price {
+    text-decoration: line-through;
+}
+
+.card-header {
+    position: relative;
+}
+
+.card-header > img {
+    display:  block;
+}
+
+.card-body p,span {
+    font-size: 14px;
+}
+
+.card-header .price-badge {
+    position: absolute;
+    bottom: 30px;
+}
+
+.card-header .tag-badge {
+    position: absolute;
+    bottom: 30px;
+    left: 45px;
+}
+
+.card-header .only{
+    position: absolute;
+    bottom: 30px;
+    left: 0px;
+}
+
+.card-header .heart-badge {
+    position: absolute;
+    top: 20px;
+    right: 0;
+}
+
+.price-badge {
+    background-color: #FF0000;
+    color: white;
+    padding: 0 5px;
+    font-size: 13px;
+}
+
+.tag-badge {
+    background-color: #008001;
+    color: white;
+    padding: 0 6px;
+    font-size: 13px;
+}
+
+.heart-badge {
+    background-color: white;
+    font-size: 20px;
+    padding: 9px;
+}
+
+.card-header .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transition: 500ms;
+}
+
+.card-header:hover .overlay {
+    opacity: 1;
+    z-index: 20;
+}
+
+
+.page-footer {
+    background-color: black;
+    color: white;
+    margin-top: 35px;
+    padding: 15px;
+}
+
+.page-footer .row {
+    justify-content: space-between;
+}
+
+
+
+/* utilities */
+
+.flex {
+    display: flex;
+}
+</style>
